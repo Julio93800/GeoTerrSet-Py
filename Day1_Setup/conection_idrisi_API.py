aaedc2743cdbd53e5d05176cbb7ec0c244fcf06f
Julio93800 <112217033+Julio93800@users.noreply.github.com>
2025-02-15 20:30:38 -0600

    Create conection_idrisi_API.py
    Added the Day 1 folder and conection_idrisi_API.py script to begin the Day 1 setup.

---
+++ Day1_Setup/conection_idrisi_API.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb 15 20:10:46 2025
+
+@author: Admin
+"""
+
+import win32com.client
+
+
+try:
+    # Intentar crear la instancia del servidor COM
+    IDRISI32 = win32com.client.Dispatch('IDRISI32.IdrisiAPIServer')
+    print("✅ Conexión exitosa con IDRISI API.")
+    
+    # Comprobar si el objeto tiene algún método o propiedad
+    #print("🔍 Métodos disponibles:", dir(IDRISI32))
+
+except Exception as e:
+    print(f"❌ Error al conectar con IDRISI: {e}")
+
+
+
+# Set IDRISI working directory path the data folder. If you are using this code, you must update the file path here. 
+IDRISI32.SetWorkingDir("C:/Users/Admin/Desktop/Terrset")
+
+IDRISI32.DisplayFile('Abril-2019/Indices/lswi_abril_2019_pol.rst', 'greyscale')
